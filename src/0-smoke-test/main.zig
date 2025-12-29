5f5933536c753a3c3f43bfbde9b7036019153488
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2025-12-28 21:07:14 -0500

    fix: this works
    - But it's using the deprecated functions
    - Going to try to use the Reader and Writer interfaces

--- src/0-smoke-test/main.zig
+++ src/0-smoke-test/main.zig
@@ -2,6 +2,12 @@ const std = @import("std");
 const builtin = @import("builtin");
 const utils = @import("utils");
 const log = utils.logger;
+const debug = std.log.debug;
+
+// Constants
+const name: []const u8 = "0.0.0.0";
+const port = 18888;
+const buff_size = 1048576;
 
 // Configure logging at the root level
 pub const std_options: std.Options = .{
@@ -17,31 +23,61 @@ pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     try log.init();
     defer log.deinit();
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-    std.log.debug("All your {s} are belong to us.", .{"codebase"});
-}
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    // Initialize allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    const allocator = gpa.allocator();
+
+    // Create server
+    var server: std.net.Server = undefined;
+    defer server.deinit();
+    {
+        const addrlist = try std.net.getAddressList(allocator, name, port);
+        defer addrlist.deinit();
+        debug("Got Addresses: '{s}'!!!", .{addrlist.canon_name.?});
+
+        for (addrlist.addrs) |addr| {
+            debug("\tTrying to listen...", .{});
+            // Not intuitive but `listen` calls `socket, bind, and listen`
+            server = addr.listen(.{}) catch continue;
+            debug("\tGot one!", .{});
+            break;
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    }
+    const cpus = try std.Thread.getCpuCount();
+    var tp: std.Thread.Pool = undefined;
+    try tp.init(.{ .allocator = allocator, .n_jobs = @as(u32, @intCast(cpus)) });
+    defer tp.deinit();
+
+    debug("ThreadPool initialized with {} capacity", .{cpus});
+    debug("We are listeninig baby!!!...", .{});
+    while (true) {
+        debug("waiting for a new connection...", .{});
+        const connection = try server.accept();
+        debug("got new connection!!!", .{});
+        try tp.spawn(handle_connection, .{connection});
+    }
+}
+
+fn handle_connection(connection: std.net.Server.Connection) void {
+    var data: [buff_size]u8 = undefined;
+    const stream = connection.stream;
+    defer stream.close();
+
+    while (true) {
+        debug("\twaiting for some data...", .{});
+        const bytes = stream.read(&data) catch 0;
+        if (bytes == 0) {
+            debug("\tERROR: error, or closing request either way... closing this connection", .{});
+            return;
+        }
+
+        debug("\treceived some bytes = {}!!", .{bytes});
+        stream.writeAll(data[0..bytes]) catch |err| {
+            debug("\tERROR: error sendAll function {}... closing this connection", .{err});
+            return;
+        };
+    }
 }
